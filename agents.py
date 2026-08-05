cb8eeb740c8970c5cfdb44bdd61173f555719c21
Ritik Mahawar <mahawarritik3@gmail.com>
2026-08-05 18:07:37 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -4,6 +4,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from tools import web_search , scrape_url 
 from dotenv import load_dotenv
+load_dotenv()
 
 llm = ChatMistralAI(model="mistral-small-latest",temperature=0)
 
